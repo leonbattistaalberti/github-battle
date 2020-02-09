a5119eb35ec84bbbc6d3d88056b550083956d818
thelonehegelian <thelonehegelian@gmail.com>
2020-02-09 15:17:15 +0000

    Add Results component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
 import PropTypes from 'prop-types'
+import Results from './Results'
 
 function Instructions () {
   return (
@@ -111,7 +112,8 @@ export default class Battle extends React.Component {
     super(props) 
     this.state = {
       playerOne: null,
-      playerTwo: null
+      playerTwo: null,
+      battle : false
     }  
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleReset = this.handleReset.bind(this)
@@ -129,7 +131,15 @@ export default class Battle extends React.Component {
   }
 
   render() {
-    const { playerOne, playerTwo } = this.state
+    const { playerOne, playerTwo, battle} = this.state
+    
+    if(battle === true) {
+    return (
+        <Results playerOne = {playerOne} playerTwo = {playerTwo}/>
+    ) 
+
+    }
+    
     return (
       <React.Fragment>
         <Instructions />
@@ -161,7 +171,16 @@ export default class Battle extends React.Component {
                     />
             } 
           </div>
+            {playerOne && playerTwo && (
+                <button 
+                    className = 'btn dark-btn btn-space'
+                    onClick = {()=>{this.setState({battle: true})}}
+                >
+                    Battle
+                </button>
+            )}
         </div>
+          
 
       </React.Fragment>
     )
